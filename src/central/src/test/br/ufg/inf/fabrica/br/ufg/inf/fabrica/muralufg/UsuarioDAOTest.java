0ff1db4f090ca9ffa5018d814fd81478e0f15551
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-11-24 00:20:40 -0200

    Criado a classe de teste UsuarioDAOTest

---
+++ src/central/src/test/br/ufg/inf/fabrica/br/ufg/inf/fabrica/muralufg/UsuarioDAOTest.java
@@ -0,0 +1,30 @@
+package br.ufg.inf.fabrica.br.ufg.inf.fabrica.muralufg;
+
+import org.junit.*;
+
+/**
+ * Created by Eurismar on 24/11/2014.
+ */
+public class UsuarioDAOTest {
+    @BeforeClass
+    public static void setUpClass(){
+
+    }
+    @AfterClass
+    public static void tearDownClass(){
+
+    }
+    @Before
+    public void setUp(){
+
+    }
+    @After
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void testUsuarioDAO(){
+
+    }
+}
